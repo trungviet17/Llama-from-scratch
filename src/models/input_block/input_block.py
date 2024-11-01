5f4a870d511d47ad276b4275152d545fb2b33482
nguyenminhquy1624 <minhquy1624@gmail.com>
2024-11-01 21:54:54 +0700

    llama : init tokenizer

--- src/models/input_block/input_block.py
+++
@@ -1,8 +0,0 @@
-"""
-Khối xử lý dữ liệu -> chuyển đổi text ban đầu thành các vector embedding 
-    1. Tokenizer text thuần thành các id 
-    2. Sử dụng model embedding để chuyển đổi 
-
-"""
-
-import torch.nn as nn 
---
+++ src/models/input_block/tokenizer.py
@@ -0,0 +1,57 @@
+from tiktoken.load import load_tiktoken_bpe
+from tiktoken import Encoding, get_encoding
+
+
+class LlamaTokenizer: 
+    
+    def __init__(self, model_path: str = "cl100k_base"): 
+        # get vocab size of tokennizer base model
+        model_base = get_encoding(model_path)
+        
+        vocab_size = len(model_base._mergeable_ranks) + len(model_base._special_tokens)
+        
+        # regex để tách từ 
+        pat_str =  r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+" 
+        
+        # setup special tokens 
+        special_tokens = ['<|beginoftext|>','<|pad_id|>']
+        
+        self.special_tokens = model_base._special_tokens
+        self.special_tokens.update({
+            tokens : i + vocab_size for i, tokens in enumerate(special_tokens) 
+        })
+        
+        self.model = Encoding(
+            name = model_path, 
+            pat_str= pat_str,
+            special_tokens = self.special_tokens, 
+            mergeable_ranks= model_base._mergeable_ranks
+        )
+        
+    def encode(self, text: str, allow_special: bool = True): 
+        allowed_special = set(self.special_tokens.keys()) if allow_special else set()
+        return self.model.encode(text, allowed_special=allowed_special)
+    
+    def decode(self, tokens: list, allow_special: bool = True): 
+        allowed_special = set(self.special_tokens.keys()) if allow_special else set()
+        return self.model.decode(tokens, allowed_special=allowed_special)
+
+
+if __name__ == "__main__": 
+    
+    def test_tokenizer(): 
+        tokenizer = LlamaTokenizer()
+        
+        print("INIT PASSED")
+        text = "Hello, this is a test"
+        tokens = tokenizer.encode(text)
+        print(tokens)
+        print('ENCODE PASSED')
+        
+        decoded_text = tokenizer.decode(tokens)
+        print(decoded_text)
+        print("DECODE PASSED")
+        
+    test_tokenizer()
+    
+    
